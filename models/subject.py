454378d84c9815a7b9cbf0a512f64b2310a171cc
Wesley <58739058+MarcoRt@users.noreply.github.com>
2023-03-03 18:20:29 -0600

    primer commit

---
+++ models/subject.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from odoo.exceptions import UserError
+
+from odoo import fields, models, api
+
+logger = logging.getLogger(__name__)
+
+class Subject(self):
+    _name = "subject"
+    _description = "Módulo materias"
---
+++ models/teacher.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from odoo.exceptions import UserError
+
+from odoo import fields, models, api
+
+logger = logging.getLogger(__name__)
+
+class Teacher(self):
+    _name = "teacher"
+    _description = "Módulo maestro"
