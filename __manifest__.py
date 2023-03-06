82cb7df144f10d1a220c02fbf1af108b85b8f9c0
Wesley <58739058+MarcoRt@users.noreply.github.com>
2023-03-06 11:38:01 -0600

    Agregué teacher_view

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,11 @@
 # -*- coding:utf-8 -*-
 
 {
-    'name' : 'School',
-    'version' : '1.0.1',
-    'depends' : [
-        'contacts',
-        'mail'
-    ],
-    'autor' : 'Marco Rodriguez',
-    'category' : 'School',
-    'description' : 'Módulo de escuela',
-    'data' : ['views/menu.xml',
-              'views/student_view.xml'
-    ]
+    "name": "School",
+    "version": "1.0.1",
+    "depends": ["contacts", "mail"],
+    "autor": "Marco Rodriguez",
+    "category": "School",
+    "description": "Módulo de escuela",
+    "data": ["views/menu.xml", "views/student_view.xml", "views/teacher_view.xml"],
 }
